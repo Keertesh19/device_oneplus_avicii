b59bdc9ec6518852335499a86290cede2e42a150
Keertesh19 <keertesh.k98@gmail.com>
2022-03-16 10:05:54 +0000

    avicii: Add assertions for recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,5 +32,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 7511998464
 BOARD_SUPER_PARTITION_SIZE := 15032385536
 
+# Assertions
+TARGET_OTA_ASSERT_DEVICE := avicii
+
 # inherit from the proprietary version
 -include vendor/oneplus/avicii/BoardConfigVendor.mk
